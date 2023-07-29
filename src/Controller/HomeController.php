c82afdfef8fdf55780ba18ecf0f00c148266fdff
MinhajulAnwar <minhaj.me.bd@gmail.com>
2023-07-29 16:54:03 +0600

    skeleton controller for scraper

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'app_home', methods:['GET', 'HEAD'])]
+    public function index(): Response
+    {
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+    #[Route('/scrape/{needle}', name: 'scrape', methods: ['GET'])]
+    public function scrape($scrape): Response {
+        return $this->json("empty");
+    }
+}

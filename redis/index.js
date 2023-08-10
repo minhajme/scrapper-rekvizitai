14522a6f1acb0244f3b9cf6cb72f194aae6194b5
MinhajulAnwar <minhaj.me.bd@gmail.com>
2023-08-10 14:36:11 +0600

    rabbitmq producer code

--- redis/index.js
+++ redis/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 require('./helpers');
 const express = require('express');
+const cors = require('cors');
 const redislib = require('redis');
 const redis = redislib.createClient({
   url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`
@@ -8,6 +9,9 @@ const redis = redislib.createClient({
 const amqplib = require('amqplib');
 
 const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/get_rows', async (req, res) => {
     if (!redis.isOpen) await redis.connect();
@@ -21,7 +25,7 @@ app.get('/get_rows', async (req, res) => {
     let results = {};
     const recordsTotal = (await redis.sendCommand(['FT.SEARCH', 'idx:companies', '*', 'NOCONTENT']))[0];
     let recordsFiltered = 0;
-    if (req.query.search.value) {
+    if (req.query.search.value) { // TODO trim
         const search_ids = req.query.search.value.replace(',', '* | *').wrap('*');
         results = await redis.ft.search('idx:companies', `@reg:${search_ids}`, {LIMIT: {from, size}});
         recordsFiltered = (await redis.sendCommand(['FT.SEARCH', 'idx:companies', `@reg:${search_ids}`, 'NOCONTENT']))[0];
@@ -33,23 +37,24 @@ app.get('/get_rows', async (req, res) => {
 
     const rows = results.documents.map(doc => doc.value);
 
-    res.setHeader('Access-Control-Allow-Origin', req.headers['origin']?req.headers['origin']:'');
-    res.setHeader('Access-Control-Allow-Headers', 'access-control-allow-origin');
-    res.setHeader('Vary', 'origin');
     return res.json({data: rows, recordsTotal, recordsFiltered});
 });
-app.get('/crawl', async (req, res) => {
+app.post('/scrape', async (req, res) => {
     const amqp_conn = await amqplib.connect(`amqp://${process.env.AMQP_HOST}`);
     const amqp_channel = await amqp_conn.createChannel();
-    await amqp_channel.assertQueue('crawler', {durable:false});
+    await amqp_channel.assertQueue('crawler-needle', {durable:true});
+     
+    const search = req.body.search; // relying on frontend to send validated needles
+    await amqp_channel.sendToQueue('crawler-needle', Buffer.from(search), {persistent: true}); 
+    setTimeout(async () => {
+        await amqp_conn.close();
+    }, 500);
     
-    res.setHeader('Access-Control-Allow-Origin', req.headers['origin']?req.headers['origin']:'');
-    res.setHeader('Access-Control-Allow-Headers', 'access-control-allow-origin');
-    res.setHeader('Vary', 'origin');
-    const search = req.query.search; // relying on frontend to send validated needles
-    await amqp_channel.sendToQueue('crawler', Buffer.from(search)); 
-    await amqp_conn.close();
-    return res.json([]); 
+    return res.json({queue: true, msgid: search}); 
+});
+app.get('/check_scrape', (req, res) => {
+    const msgid = req.body.msgid;
+    return res.json({acked: false});
 });
 
 app.listen(3000, () => {
